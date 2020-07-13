2a9b61216572cf4c3bede3489a52c8efbb2446a6
Oscar Mier <voscarmv@gmail.com>
2020-07-13 16:06:42 -0500

    Wrote addBook() and render()

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
-function Book(title, author) {
+let books = []
+
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
-  this.pages = 0;
+  this.pages = pages;
   this.read = false;
   this.info = function () {
     if (this.read) {
@@ -11,7 +13,26 @@ function Book(title, author) {
   };
 }
 
-const book1 = new Book("Lazarillo de Tormes", "Anonimo");
-book1.pages = 300;
-book1.read = true;
-console.log(book1.info());
+function addBook(title, author, pages){
+  books.push(new Book(title, author, pages));
+}
+
+function render(books){
+  books.forEach(
+    function(b){
+      // Output to HTML
+      console.log(b.info());
+    }
+  );
+}
+
+// const book1 = new Book("Lazarillo de Tormes", "Anonimo");
+// book1.pages = 300;
+// book1.read = true;
+//console.log(book1.info());
+
+addBook("Lazarillo de Tormes1", "Anonimo1", 100);
+addBook("Lazarillo de Tormes2", "Anonimo2", 200);
+addBook("Lazarillo de Tormes3", "Anonimo3", 300);
+
+render(books);
