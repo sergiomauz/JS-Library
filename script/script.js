da2aa7ccf4c2de64d7eb41504798dd64f45b8e90
sergiomauz <sergio.mauz88@gmail.com>
2020-07-14 09:00:11 -0500

    Improved user interface using containers and nav

--- script/script.js
+++ script/script.js
@@ -1,91 +1,75 @@
 let books = [];
 
-function showForm() {
-  document.getElementById("frmNewBook").className = "card";
-  document.getElementById("btnNewBook").innerHTML = "Cancel";
-  document.getElementById("btnNewBook").addEventListener("click", function () {
-    hideForm();
-  });
-}
-
-function hideForm() {
-  document.getElementById("frmNewBook").className = "card d-none";
-  document.getElementById("btnNewBook").innerHTML = "New Book";
-  document.getElementById("btnNewBook").addEventListener("click", function () {
-    showForm();
-  });
-}
-
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    if (this.read) {
-      return `<div class="card-body">
-      <h5 class="card-title">${this.title}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${this.author}</h6>
-      <p class="card-text">
-      ${this.pages} pages, already read.
-      </p>
-    </div>`;
+  this.info = () => {
+    let wasRead = 'already read';
+    if (!this.read) {
+      wasRead = 'not read yet';
     }
-    return `<div class="card-body">
-      <h5 class="card-title">${this.title}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${this.author}</h6>
-      <p class="card-text">
-      ${this.pages} pages, not read yet.
-      </p>
-    </div>`;
+    return `<div class='card-body'>
+              <h5 class='card-title'>${this.title}</h5>
+              <h6 class='card-subtitle mb-2 text-muted'>${this.author}</h6>
+              <p class='card-text'>
+                ${this.pages} pages, ${wasRead}.
+              </p>
+            </div>`;
   };
+}
+
+function altForm() {
+  const formStyle = document.getElementById('frmNewBook').className;
+  if (formStyle.includes('d-none')) {
+    document.getElementById('frmNewBook').className = 'card mb-3';
+    document.getElementById('btnNewBook').innerHTML = 'Cancel';
+  } else {
+    document.getElementById('frmNewBook').className = 'card mb-3 d-none';
+    document.getElementById('btnNewBook').innerHTML = 'New Book';
+  }
+}
+
+function render(books) {
+  let html = '';
+  books.forEach((b, i) => {
+    html += `<div data-attribute='book_${i}' class='card m-1 bg-light' style='width: 15rem;'>
+              ${b.info()}
+            </div>`;
+  });
+  document.getElementById('books_list').innerHTML = html;
+}
+
+function renderLast(books) {
+  const html = `<div data-attribute='book_${books.length - 1}' class='card m-1 bg-light' style='width: 15rem;'>
+                  ${books[books.length - 1].info()}
+                </div>`;
+  document.getElementById('books_list').innerHTML += html;
+  altForm();
 }
 
 function addBook(title, author, pages, read) {
   books.push(new Book(title, author, pages, read));
 }
 
-function render(books) {
-  let html = "";
-  books.forEach(function (b, i) {
-    // Output to HTML
-    html += `<div data-attribute="book_${i}" class="card" style="width: 18rem;">
-    ${b.info()}
-    </div>`;
-  });
-  document.getElementById("books_list").innerHTML = html;
-}
-
-function renderLast(books){
-   let html = `<div data-attribute="book_${books.length - 1}" class="card" style="width: 18rem;">
-   ${books[books.length - 1].info()}
-   </div>`;
-   document.getElementById("books_list").innerHTML = html;
-}
-
-// const book1 = new Book("Lazarillo de Tormes", "Anonimo");
-// book1.pages = 300;
-// book1.read = true;
-//console.log(book1.info());
-
-addBook("Lazarillo de Tormes1", "Anonimo1", 100, false);
-addBook("Lazarillo de Tormes2", "Anonimo2", 200, true);
-addBook("Lazarillo de Tormes3", "Anonimo3", 300, false);
-
-render(books);
-
-document.getElementById("btnNewBook").addEventListener("click", function () {
-  showForm();
+document.getElementById('btnNewBook').addEventListener('click', () => {
+  altForm();
 });
 
-document
-.getElementById("addBook")
-.addEventListener("click", function () {
-  let title = document.getElementById("txtTitle").value;
-  let author = document.getElementById("txtAuthor").value;
-  let pages = document.getElementById("txtPages").value;
-  let read = document.getElementById("chkRead").checked;
+document.getElementById('btnAddBook').addEventListener('click', () => {
+  const title = document.getElementById('txtTitle').value;
+  const author = document.getElementById('txtAuthor').value;
+  const pages = Number.parseInt(document.getElementById('txtPages').value, 10);
+  const read = document.getElementById('chkRead').checked;
 
   addBook(title, author, pages, read);
   renderLast(books);
 });
+
+addBook('Lazarillo de Tormes', 'Anonymous', 100, false);
+addBook('Divine Comedy', 'Dante Alighieri', 200, true);
+addBook('Object-Oriented Programming', 'Luis Joyanes Aguilar', 300, false);
+addBook('Data Structures', 'Osvaldo Cairo', 200, true);
+
+render(books);
